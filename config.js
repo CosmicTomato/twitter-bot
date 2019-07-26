2d69e457009eff640fe92ca4557a289104e80165
JC <Jeffrey@Jeffrey-Commonss-MacBook-Pro.local>
2019-07-25 21:18:21 -0700

    twitter bot

---
+++ config.js
@@ -0,0 +1,8 @@
+//config.js
+
+module.exports = {
+  consumer_key: 'kYjBkDQL5RaCWLpvEYgJmuStO',
+  consumer_secret: 'KgJgay6hELHFyPmYg0xOWaF4tBMOJSobu0naVPQE5peqFyPEGU',
+  access_token_key: '1154560344078970880-Fcl9iOrCApAaGXmY230Ra5aopg2HeD',
+  access_token_secret: 'mLriJZk9f6m2vIT6o19HnWQDTMuWdcH2d5uehe5Y4YzuZ'
+}
